e904d214f5039952f606861056b2cc8b4f223a3d
zacknesz <exedickson@gmail.com>
2025-04-08 06:52:32 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,6 @@ import os
 async def start(update, context):
     await update.message.reply_text("Hola! Soy un bot.")
 
-app = ApplicationBuilder().token(os.environ["7745916264:AAFaxmVrQsqiEjq5yhq6BdDQ7wBKjjb4Gn8"]).build()
+app = ApplicationBuilder().token(os.environ["7760745441:AAHuqxfBvskKyvA3gzflIe8tUYuU_fCXiKg"]).build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
