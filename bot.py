9b62492c8a26e639f81c9601631b761e5c42f9f2
zacknesz <exedickson@gmail.com>
2025-04-09 09:01:46 -0400

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
-import os
-
-async def start(update, context):
-    await update.message.reply_text("Hola! Soy un bot.")
-
-app = ApplicationBuilder().token(os.environ["7760745441:AAHuqxfBvskKyvA3gzflIe8tUYuU_fCXiKg"]).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
